12c38aa6cf04516180bb2b6cb0f61a299ca1dcef
daria.kochanova@anuitex.com <daria.kochanova@anuitex.com>
2020-02-23 12:11:36 +0200

    init

---
+++ precache-manifest.6083e75ccc164294bd11b0d74e2e474f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26068c7cc5388a83c42b9930ea15c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1594dfb19cf06b0a4045",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b312fd30b894c153f985",
+    "url": "/static/js/2.bb3b8b95.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bb3b8b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1594dfb19cf06b0a4045",
+    "url": "/static/js/main.dfeef8b6.chunk.js"
+  },
+  {
+    "revision": "f083377de636a50c6a72",
+    "url": "/static/js/runtime-main.35536eee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
